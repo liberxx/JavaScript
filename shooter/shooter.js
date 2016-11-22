8b68d2d5358c0f4a044ae850c9165877f7640b93
Andrey <ritorx@gmail.com>
2016-11-22 12:58:15 +0300

    learning process

---
+++ shooter/shooter.js
@@ -0,0 +1,100 @@
+﻿//Запрашиваем имя игрока
+var player = prompt("Введите своё имя", "Игрок");
+//Объявляем переменные уничтожаемого объекта и игрового поля
+
+var ball = document.getElementById("ball");
+var ball2 = document.getElementById("ball2");
+var ball3 = document.getElementById("ball3");
+
+var ballons = [ball, ball2, ball3]
+
+var body = document.getElementsByTagName("body");
+
+
+//Массив стартовых координат по Х.
+var coordinates = [0, 100, 200, 300, 400, 500, 600, 700]
+
+var scoreboard = document.getElementById("score");
+
+//Счетчик попаданий
+var count = 0;
+
+//Стартовая позиция шарика
+var positions_y = [700, 700, 700];
+
+function move(obj) {
+
+
+	if (positions_y[i] < -200) {
+		var position_x = coordinates[Math.floor(Math.random()*coordinates.length)];
+		positions_y[i] = 700;
+		obj.style.left = position_x + "px";
+	}	
+			positions_y[i] -= 3;
+			obj.style.top = positions_y[i] + "px";
+			
+}
+
+//При попадании перемещаем объект за нижнюю границу экрана и увеличиваем переменную count.
+
+for (i = 0, len = ballons.length; i < len; i++){
+	
+    ballons[i].onclick = function() {
+
+	var position_x = coordinates[Math.floor(Math.random()*coordinates.length)];
+
+	for (i = 0, len = ballons.length; i < len; i++){
+		if (ballons[i].id == this.id) {
+			ballons[i].style.left = position_x + "px";
+			positions_y[i] = 700;
+		}	
+	}	
+	
+	count += 1;
+	scoreboard.innerHTML = count;
+	}
+}
+
+//Вызов функции движения для каждого из шаров.
+timer_id = setInterval(function(){
+	for (i = 0, len = ballons.length; i < len; i++)
+		move.call({}, ballons[i]);
+	}, 10);
+	
+setTimeout(function() {
+  clearInterval(timer_id);
+},60000);	
+
+//Если время закончилось - очищаем body и выводим результат игры.
+function timer(){
+var obj=document.getElementById('timer');
+obj.innerHTML--;
+    if (obj.innerHTML==0){
+        setTimeout(function(){},1000);
+		body[0].innerHTML = '';
+		
+		var result = document.createElement("div");
+		result.id = "scroll_container"
+		body[0].appendChild(result);
+		
+		var scrl = document.createElement("img");
+		scrl.src = "svitok.jpg";
+		scrl.id = "scroll";
+		result.appendChild(scrl);
+		
+		var result_text_container = document.createElement("div");
+		result_text_container.id = "result_text_container"
+		body[0].appendChild(result_text_container);
+		
+		
+		var scrl_text = document.createElement("p");
+		scrl_text.innerHTML = player + "," + "Ваш результат:" + " " + count;
+		scrl_text.id = "scroll_text";
+		result_text_container.appendChild(scrl_text);
+		
+    } else {
+        setTimeout(timer,1000);
+    }
+}
+setTimeout(timer,1000);
+	
---
+++ table-sort/sort.js
@@ -0,0 +1,62 @@
+﻿// Получаем объект по ID.
+var tablica = document.getElementById('tablica');
+// Переменная статуса сортировки. Возрастающая/убывающая
+var ascending = false
+
+//Обработчик при клике. В случае клика по TH производим вызов функции Sort.
+tablica.onclick = function(event){
+    if (event.target.tagName != 'TH') return;
+	
+	// Переключение режима сортировки.
+	if (ascending == false) 
+		ascending = true;
+	else
+		ascending = false;		
+	
+    // Если TH - сортируем
+    sort(event.target.cellIndex, event.target.getAttribute('data-type'));
+};
+
+function sort(colNum, type) {
+      var tbody = tablica.getElementsByTagName('tbody')[0];
+
+      // Составить массив из TR
+      var rowsArray = [].slice.call(tbody.rows);
+
+      // определить функцию сравнения, в зависимости от типа
+      var compare;
+
+      switch (type) {
+        case 'number':
+          compare = function(rowA, rowB) {
+            return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+          };
+          break;
+        case 'string':
+          compare = function(rowA, rowB) {
+            return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+          };
+          break;
+      }
+
+      // сортировать по возрастанию/убыванию
+	  if (ascending == true)
+		rowsArray.sort(compare);		
+	  else  
+		rowsArray.sort(compare).reverse();
+	  
+
+      // Убрать tbody из большого DOM документа для лучшей производительности
+      tablica.removeChild(tbody);
+
+      // добавить результат в нужном порядке в TBODY
+      // они автоматически будут убраны со старых мест и вставлены в правильном порядке
+      for (var i = 0; i < rowsArray.length; i++) {
+        tbody.appendChild(rowsArray[i]);
+      }
+
+      tablica.appendChild(tbody);
+
+}
+
+
