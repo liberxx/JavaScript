94c9a3ab4eb5d743af0027fbc3b1ea0ac356cb34
Andrey <ritorx@gmail.com>
2016-11-24 18:47:30 +0300

    updated shooter

--- shooter/shooter.js
+++ shooter/shooter.js
@@ -1,20 +1,21 @@
-﻿//Запрашиваем имя игрока
+//Запрашиваем имя игрока
 var player = prompt("Введите своё имя", "Игрок");
 //Объявляем переменные уничтожаемого объекта и игрового поля
 
-var ball = document.getElementById("ball");
-var ball2 = document.getElementById("ball2");
-var ball3 = document.getElementById("ball3");
+var ball = document.getElementsByClassName("ball");
+var ball2 = document.getElementsByClassName("ball2");
+var ball3 = document.getElementsByClassName("ball3");
 
-var ballons = [ball, ball2, ball3]
+//var ballons = document.querySelector(".ball")
+var ballons = [ball[0], ball2[0], ball3[0]];
 
 var body = document.getElementsByTagName("body");
 
 
 //Массив стартовых координат по Х.
-var coordinates = [0, 100, 200, 300, 400, 500, 600, 700]
+var coordinates = [0, 100, 200, 300, 400, 500, 600, 700];
 
-var scoreboard = document.getElementById("score");
+var scoreboard = document.getElementsByClassName("score");
 
 //Счетчик попаданий
 var count = 0;
@@ -24,41 +25,40 @@ var positions_y = [700, 700, 700];
 
 function move(obj) {
 
-
-	if (positions_y[i] < -200) {
+	if (positions_y[c] < -200) {
 		var position_x = coordinates[Math.floor(Math.random()*coordinates.length)];
-		positions_y[i] = 700;
+		positions_y[c] = 700;
 		obj.style.left = position_x + "px";
 	}	
-			positions_y[i] -= 3;
-			obj.style.top = positions_y[i] + "px";
+			positions_y[c] -= 3;
+			obj.style.top = positions_y[c] + "px";
 			
 }
 
 //При попадании перемещаем объект за нижнюю границу экрана и увеличиваем переменную count.
 
-for (i = 0, len = ballons.length; i < len; i++){
+for (var i = 0, len = ballons.length; i < len; i++){
 	
     ballons[i].onclick = function() {
 
 	var position_x = coordinates[Math.floor(Math.random()*coordinates.length)];
 
-	for (i = 0, len = ballons.length; i < len; i++){
-		if (ballons[i].id == this.id) {
-			ballons[i].style.left = position_x + "px";
-			positions_y[i] = 700;
+	for (var b = 0, len = ballons.length; b < len; b++){
+		if (ballons[b].className == this.className) {
+			ballons[b].style.left = position_x + "px";
+			positions_y[b] = 700;
 		}	
 	}	
 	
 	count += 1;
-	scoreboard.innerHTML = count;
+	scoreboard[0].innerHTML = count;
 	}
 }
 
 //Вызов функции движения для каждого из шаров.
 timer_id = setInterval(function(){
-	for (i = 0, len = ballons.length; i < len; i++)
-		move.call({}, ballons[i]);
+	for (c = 0, len = ballons.length; c < len; c++) {
+		move.call({}, ballons[c]); }
 	}, 10);
 	
 setTimeout(function() {
@@ -74,22 +74,22 @@ obj.innerHTML--;
 		body[0].innerHTML = '';
 		
 		var result = document.createElement("div");
-		result.id = "scroll_container"
+		result.className = "scroll_container"
 		body[0].appendChild(result);
 		
 		var scrl = document.createElement("img");
 		scrl.src = "svitok.jpg";
-		scrl.id = "scroll";
+		scrl.className = "scroll";
 		result.appendChild(scrl);
 		
 		var result_text_container = document.createElement("div");
-		result_text_container.id = "result_text_container"
+		result_text_container.className = "result_text_container"
 		body[0].appendChild(result_text_container);
 		
 		
 		var scrl_text = document.createElement("p");
 		scrl_text.innerHTML = player + "," + "Ваш результат:" + " " + count;
-		scrl_text.id = "scroll_text";
+		scrl_text.className = "scroll_text";
 		result_text_container.appendChild(scrl_text);
 		
     } else {
@@ -97,4 +97,3 @@ obj.innerHTML--;
     }
 }
 setTimeout(timer,1000);
-	
