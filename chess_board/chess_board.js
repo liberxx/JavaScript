8b876a1e5e71f1c03e9ee6235b5060498758421d
Andrey <ritorx@gmail.com>
2016-12-09 20:37:52 +0300

    Решение chessboard

---
+++ chess_board/chess_board.js
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, создающую строку, содержащую решётку, в которой линии разделяются символами	новой строки. На каждой	позиции
+либо пробел, либо #. В результате должна получиться шахматная доска. Размер доски переменный.
+*/
+
+var h_lines = prompt("Enter horizontal lines number","");
+var v_lines = prompt("Enter vertical lines number","");
+var chess_board = "";
+var i = 1;
+while (i <= v_lines) {
+	if (i % 2 == 0) {
+  	var n = 2;
+  }
+  else {
+  	var n = 1;
+  }
+	while (n <= h_lines) {
+  	if (n % 2 !=0) {
+    	chess_board += " ";
+    }
+    else {
+    	chess_board += "#";
+    }
+    if (n == h_lines) {
+    	chess_board += "\n";
+    }
+  	n++;
+  }
+  i++;
+}
+console.log(chess_board);
